3e41953162d18165da6f7cf0a4bde2b3965374ae
Devon <lewisd@bu.edu>
2021-04-03 00:32:32 -0400

    Add an integer parser

--- interpreter.ml
+++ interpreter.ml
@@ -28,20 +28,6 @@ let (>>=) : 'a parser -> ('a -> 'b parser) -> 'b parser =
     |
       Some (a, rest) -> f a rest
 
-(* Applies p zero or more times until it fails
-   Returns a list of each result of p on ls *)
-let rec many (p : 'a parser) : ('a list) parser =
-  fun ls ->
-  match p ls with
-    None -> Some ([], ls)
-  |
-    Some (x, ls) -> begin
-    match many p ls with
-      None -> Some (x :: [], ls)
-    |
-      Some (xs, ls) -> Some (x :: xs, ls)
-  end
-
 (* Does not consume the input string
    Returns c *)
 let return (a: 'a) : 'a parser =
@@ -55,16 +41,44 @@ let zero : 'a parser =
 let parse (p: 'a parser) (str: string) : ('a * char list) option =
   p (explode str)
 
+(* Applies p zero or more times until it fails
+   Returns a list of each result of p on ls *)
+let rec many (p : 'a parser) : ('a list) parser =
+  fun ls ->
+  match p ls with
+    None -> Some ([], ls)
+  |
+    Some (x, ls) -> begin
+    match many p ls with
+      None -> Some (x::[], ls)
+    |
+      Some (xs, ls) -> Some (x::xs, ls)
+  end
+
+(* Applies p 1 or more times until it fails
+   Returns a list of each result of on ls *)
+let rec many1 (p : 'a parser) : ('a list) parser =
+  fun ls ->
+  match p ls with
+    Some (x, ls) -> begin
+      match many p ls with
+        Some (xs, ls) -> Some (x::xs, ls)
+      |
+        None -> Some ([x], ls)
+    end
+  |
+    None -> None
+
 (* Parses the first character of the input *)
-let char : char parser =
+let charp : char parser =
   fun ls ->
   match ls with
-  | x :: ls -> Some (x, ls)
+  | x::ls -> Some (x, ls)
   | _ -> None
 
 (* Takes char predicate f and if f is true, parse the character *)
 let sat (f: char -> bool) : char parser =
-  char >>= fun x ->
+  charp >>= fun x ->
   if f x then return x else zero
 
 (* Takes a char, and parses that char, fail otherwise if it's not in
@@ -72,15 +86,48 @@ let sat (f: char -> bool) : char parser =
 let satc (c: char) : char parser =
   sat (fun x -> if x = c then true else false)
 
+(* Takes a char list and parses that char list, fail otherwise if it's not in
+   the input *)
+let clistp : char list -> char list parser =
+  fun inp ->
+  fun ls ->
+  let rec aux inp ls =
+    match inp, ls with
+      [], _ -> Some ([], ls)
+    |
+      c::inp_rest, c'::ls_rest ->
+      if c = c' then
+        match aux inp_rest ls_rest with
+          Some (_, rest) -> Some (inp, rest)
+        |
+          None -> None
+      else None
+    |
+     _ -> None
+  in aux inp ls
+
 (* Takes a string and parses that string, fail otherwise if it's not in
    the input *)
-let sats (str: string) : str parser =
-  if str = "" then zero else
-    let rec aux ls =
-      match ls with
-        c :: rest -> satc 'c' >>= fun _ -> aux rest
-      |
-        None -> zero 
+let sats (str : string) : string parser =
+  clistp (explode str) >>= fun x -> return (implode x)
+
+(* Parses a digit represented as a character *)
+let digitp : char parser =
+  charp >>= fun x ->
+  if '0' <= x && x <= '9'
+  then return x
+  else zero
+
+(* Parses an integer *)
+let intp : int parser =
+  begin
+  satc '-' >>= fun _ ->
+  many1 digitp >>= fun x ->
+  return (-1 * (int_of_string (implode x)))
+end <|> begin
+  many1 digitp >>= fun x ->
+  return (int_of_string (implode x))
+end
 
 let interpreter (s : string) : string list * int = failwith "undefined"
 
